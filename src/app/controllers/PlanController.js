1a6a8992844daf906d210dfe2e082e76962ab318
Edison <mage1232010@gmail.com>
2019-11-27 20:20:22 -0300

    Erro corrigido

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -6,7 +6,7 @@ class PlanController {
   async index(req, res) {
     const { page = 1, quantity = 20 } = req.query;
 
-    const plans = await Plan.finddAll({
+    const plans = await Plan.findAll({
       limit: quantity,
       offset: (page - 1) * quantity,
       order: ['price'],
